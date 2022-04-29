8e289482f79b3c9d1029f3ffddf8c26ceba5c823
Kartik-Vyas <101496656+Kartik-Vyas@users.noreply.github.com>
2022-04-29 23:59:41 +0530

    Create SettingName.java

---
+++ SettingName.java
@@ -0,0 +1,21 @@
+
+class Custom extends Thread
+{
+    public void run()
+    {
+        System.out.println("thread task");
+        Thread.currentThread().setName("Programmer thread");
+        System.out.println(Thread.currentThread().getName());
+
+    }
+}
+public class SettingName {
+    public static void main(String[] args) {
+        System.out.println("Before name change  " + Thread.currentThread().getName());
+        Custom thread = new Custom();
+        thread.start();
+        Thread.currentThread().setName("Main thread renamed by setName method");
+        System.out.println("After name change  " + Thread.currentThread().getName());
+        System.out.println(thread.isAlive());
+    }
+}
